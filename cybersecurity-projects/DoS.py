cd9df434a5fa5cce1277b7c29d88a0757973cc84
Ryan Longoria <89551302+Ryan-Longoria@users.noreply.github.com>
2022-10-20 09:14:36 -0500

    Added functions
    Each have a placeholder print statement

--- cybersecurity-projects/DoS.py
+++ cybersecurity-projects/DoS.py
@@ -1,1 +1,25 @@
+from scapy.all import *
 
+# Multiple IPs, Multiple Ports
+def multipleMultipl():
+  printf("Multiple IPs Multiple ports")
+
+# Multiple IPs, Single Port
+def multipleSingle():
+  printf("Multiple IPs Single port")
+
+# Single IP, Multiple Ports
+def singleMultiple():
+  printf("Single IP Multiple ports")
+
+# Single IP, Single Port
+def singleSinglw():
+  printf("Single IP Single port")
+
+# Main (initial) function
+def main():
+  printf("main")
+  
+# Calls main 
+if __name__ == "__main__":
+    main()
